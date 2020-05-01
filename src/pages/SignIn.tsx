c13631cb20a6791a9c8fb1566a6dc2e5c4d7325b
Pablo Satler <pablosatler@gmail.com>
2020-05-01 16:02:59 -0300

    feat: add the button displayed when the SignIn button is tapped
    - also add a cross (X) to close

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+import { Text, View, StyleSheet, Image, Dimensions, TextInput } from 'react-native';
 
 // import { Container } from './styles';
 import Animated, { Easing } from 'react-native-reanimated'
@@ -20,7 +20,8 @@ const {
   timing,
   clockRunning,
   interpolate,
-  Extrapolate
+  Extrapolate,
+  concat, // to pass the deg for rotating the cross
 } = Animated;
 
 const { width: windowWidth, height: windowHeight } = Dimensions.get('window')
@@ -68,7 +69,7 @@ export default class pages extends Component {
           block([
             cond(
               eq(state, State.END), 
-              set(this.buttonOpacity, 
+              set(this.buttonOpacity, // set opacity with the help of runTiming function
                 runTiming(new Clock(), 1, 0))
                 )
               ])
@@ -86,6 +87,30 @@ export default class pages extends Component {
     this.bgY = interpolate(this.buttonOpacity, {
       inputRange: [0, 1],
       outputRange: [-windowHeight / 3, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    // for the text input the shows up after the animation ends
+    this.textInputZIndex = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [1, -1],
+      extrapolate: Extrapolate.CLAMP,
+    });
+    this.textInputY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [0, 100],
+      extrapolate: Extrapolate.CLAMP,
+    });
+    this.textInputOpacity = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [1, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    // rotating the X cross of the close button
+    this.rotateCross = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [180, 360], // we can't pass degrees here with reanimated, so we'll the method 'concat'
       extrapolate: Extrapolate.CLAMP,
     });
   }
@@ -136,6 +161,63 @@ export default class pages extends Component {
           }} >
           <Text style={{...styles.buttonText, color: 'white'}} >SIGN IN WITH FACEBOOK</Text>
         </Animated.View>
+        
+        {/* Below are the elements shown when the buttons fade out */}
+        <Animated.View 
+          style={{
+            zIndex: this.textInputZIndex,
+            opacity: this.textInputOpacity,
+            transform: [
+              {
+                translateY: this.textInputY,
+              }
+            ],
+            height: windowHeight / 3,
+            ...StyleSheet.absoluteFill,
+            top: null,
+            justifyContent: 'center'
+          }}
+        >
+          <TapGestureHandler >
+            <Animated.View
+              style={styles.closeButton}
+            >
+              <Animated.Text
+                style={{ 
+                  fontSize: 15,
+                  transform: [
+                    {
+                      rotate: concat(this.rotateCross, 'deg')
+                    }
+                  ]
+                }}
+              >
+                X
+              </Animated.Text>
+            </Animated.View>
+          </TapGestureHandler>
+
+          <TextInput
+            placeholder="email"
+            style={styles.textInput}
+            placeholderTextColor="black"
+          />
+          <TextInput
+            placeholder="password"
+            style={styles.textInput}
+            placeholderTextColor="black"
+          />
+
+          <Animated.View 
+            style={styles.button}
+          >
+            <Text style={styles.buttonText} >
+              SIGN IN
+            </Text>
+          </Animated.View>
+
+        </Animated.View>
+
       </View>
     </View>
     );
@@ -169,11 +251,42 @@ const styles = StyleSheet.create({
     marginVertical: 5,
     borderRadius: 35,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+
+    shadowOffset: { width: 2, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
   },
   buttonText: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
 
+  textInput: {
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 0.5,
+    marginHorizontal: 20,
+    paddingLeft: 10,
+    marginVertical: 5,
+    borderColor: 'rgba(0, 0, 0, 0.2)'
+  },
+
+  closeButton: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    position: 'absolute',
+    top: -20,
+    left: (windowWidth / 2) - 20,
+    shadowOffset: { width: 2, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    // borderWidth: 1,
+    // borderColor: 'black'
   }
 })
